c814728f87e1cee82dbc9e330ef14d8ab14dc26f
Sta-ces <staescedric@gmail.com>
2017-12-25 23:08:02 +0100

    Life Function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,14 +11,32 @@ public class GameManager : MonoBehaviour
 	    [Header("Experiences:")]
 	    public Button m_buttonExperiences;
 	    public Text m_locationExperiences;
+	    [Range(10,200)]
 	    public int m_experienceMax = 100;
 
-	    [Header("Level:")]
+	    [Header("Levels:")]
 	    public Text m_locationLevel;
+	    [Range(1,200)]
+	    public int m_levelUpExperience = 100;
+
+	    [Header("Lifes:")]
+	    public Text m_locationLifeEnemi;
+	    public Text m_locationLifePlayer;
+	    [Range(1,100)]
+	    public int m_lifePlayerMax = 50;
+	    public bool m_displayMaxLife = false;
 
     #endregion
 
     #region Public void
+
+	    public void Attack(){
+	    	Debug.Log("Attack");
+	    }
+
+	    public void Potions(){
+	    	Debug.Log("Potions");
+	    }
 
     #endregion
 
@@ -30,15 +48,17 @@ public class GameManager : MonoBehaviour
             m_experiencesPlayer = m_classExperiences.ExperiencePlayer;
             m_displayGame = new DisplayGame();
             m_calcul = new Calcul();
+            m_lifePlayer = m_lifePlayerMax;
     	}
 	
 	    void Update()
         {
             ExperiencesFunction();
             LevelFunction();
+            LifeFunction();
         }
 
-        private void OnGUI()
+        void OnGUI()
         {
             if (GUI.Button(new Rect(0, Screen.height - 25, 75, 25), "+10xp"))
                 m_experiencesPlayer += 12.47f;
@@ -58,9 +78,24 @@ public class GameManager : MonoBehaviour
         	if(m_experiencesPlayer >= m_experienceMax){
         		m_levelPlayer++;
         		m_experiencesPlayer -= m_experienceMax;
-        		m_experienceMax += m_experienceMax / 2;
+        		m_experienceMax += m_levelUpExperience;
         	}
         	m_displayGame.DisplayText(m_locationLevel, m_levelPlayer.ToString());
+        }
+
+        private void LifeFunction(){
+        	m_lifeEnemi = 120;
+        	string displayLife;
+        	
+        	if(m_displayMaxLife){
+        		displayLife = m_lifePlayer.ToString()+" / "+m_lifePlayerMax.ToString();
+        	}
+        	else{
+        		displayLife = m_lifePlayer.ToString();
+        	}
+
+        	m_displayGame.DisplayText(m_locationLifePlayer, displayLife);
+        	m_displayGame.DisplayText(m_locationLifeEnemi, m_lifeEnemi.ToString());
         }
 
     #endregion
@@ -72,6 +107,8 @@ public class GameManager : MonoBehaviour
         private DisplayGame m_displayGame;
         private Calcul m_calcul;
         private int m_levelPlayer = 1;
+        private int m_lifePlayer;
+        private int m_lifeEnemi;
 
     #endregion
 }
