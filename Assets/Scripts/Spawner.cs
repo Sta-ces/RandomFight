330535faa579270aa3d9b8de6c0c389294b37dfe
Staces <staescedric@gmail.com>
2018-01-29 12:55:46 +0100

    Spawner Script and ThirdCameraPerson

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform m_SpawnerLocation;
+    public GameObject m_Player;
+
+    private void Awake()
+    {
+        m_tallOfPlayer = m_Player.transform.lossyScale.y;
+    }
+
+    private void Start()
+    {
+        Vector3 playerPosition = m_SpawnerLocation.transform.position;
+        playerPosition.y += m_tallOfPlayer;
+
+        m_Player.transform.position = playerPosition;
+    }
+
+    private float m_tallOfPlayer;
+}
---
+++ Assets/ThirdPersonPlayer/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public GameObject m_Player;
+    public Camera m_Camera;
+    public Vector3 m_PositionCamera = new Vector3(0f,4f,-4f);
+    public Vector3 m_RotationCamera = new Vector3(30f,0f,0f);
+
+    private void Awake()
+    {
+        if (m_Player == null)
+            m_Player = gameObject;
+
+        if (m_Camera == null)
+            m_Camera = Camera.main;
+    }
+
+    private void Start()
+    {
+        // Set on parent to Player
+        m_Camera.transform.parent = m_Player.transform;
+
+        m_Camera.transform.localPosition = m_PositionCamera;
+        m_Camera.transform.localRotation = Quaternion.Euler(m_RotationCamera);
+    }
+}
